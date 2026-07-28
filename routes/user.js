7c367468be49f99569234079b4e871dbe7a408b5
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-07-28 19:40:15 +0530

    bulk added

--- routes/user.js
+++ routes/user.js
@@ -86,4 +86,33 @@ Router.put("/",authmiddleware,async(req,res)=>{
         console.log ("user update ")
     )
 })
+
+
+Router.get("/bulk", async (req, res) => {
+    const filter = req.query.filter ||"";
+
+    const users = await User.find({
+        $or: [
+            {
+                firstName: {
+                    $regex: filter
+                }
+            },
+            {
+                lastName: {
+                    $regex: filter
+                }
+            }
+        ]
+    });
+
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            id: user._id
+        }))
+    });
+});
 module.exports = Routes;
