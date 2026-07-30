bb229dde42eafd991da64b7a59c6d3a9d9bd9198
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-07-30 18:24:18 +0530

    authmiddlewarer fixed

--- authmiddleware.js
+++ authmiddleware.js
@@ -1,26 +1,30 @@
 const SECRET_KEY = "your_secret_key"
 const jwt = require ("jsonwebtoken")
 
-function authmiddleware(req,res,next ){
+function authmiddleware (req ,res, next ) {
     const token = req.headers.authorization
 
     if(!token){
-        return res.status(403).json
-        console.log("token is missing")
+        return res.status(403).json ({
+        message : "token is missing"
+        })
     }
+
 
     const decoded = jwt.verify(token,SECRET_KEY)
-    const username  = decoded.userid 
+    const userid  = decoded.userid 
 
-    if (!username){
-        return res.status (403).json 
-        console.log ("invalid token")
+    if (!userid){
+        return res.status (403).json ({
+        message : ("invalid token")
+        })
     }
 
-    req.userid= userid
+    req.userid = userid ;
 
-    next()
+    next();
 }
 
-module.exports =
+module.exports = {
     authmiddleware
+};
