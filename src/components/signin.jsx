31d7bcf4480c8008f23276fe519f217289a7c6ff
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-01 19:55:26 +0530

    added dashboard

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,7 +1,13 @@
+import react from "react"
+
 function Signin (){
  
     return (
+        
     <div style={{textAlign : "center" ,padding : "10",margin :"20"}}>
+        <h1>
+            signin page 
+        </h1>
         <div>
             <input type = "text" placeholder="username"/>
         </div>
