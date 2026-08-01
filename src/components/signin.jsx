caa8123d268bdc4767d0ff5bfbacf72e05fa10e5
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-01 19:40:59 +0530

    frotend added

---
+++ src/components/signin.jsx
@@ -0,0 +1,18 @@
+function Signin (){
+ 
+    return (
+    <div style={{textAlign : "center" ,padding : "10",margin :"20"}}>
+        <div>
+            <input type = "text" placeholder="username"/>
+        </div>
+        <div>
+            <input type = "text" placeholder="password"/>
+        </div>
+        <div>
+            <button>LOG IN</button>
+        </div>
+    </div>
+)
+}
+
+export default Signin 
---
+++ src/components/signup.jsx
@@ -0,0 +1,22 @@
+import react from "react"
+
+function Signup (){
+    return (
+    <div> 
+        <h1>
+            signup page
+        </h1>
+        <div>
+            <input type="text" placeholder="FIRST NAME"/>
+        </div>
+        <div>
+            <input type="text" placeholder="LAST NAME" />
+        </div>
+        <div>
+            <button>submit</button>
+        </div>
+    </div>
+        )
+}
+
+export default Signup
